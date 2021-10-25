8eeee622577907b3d0f2d78b29365eeefc8321eb
oleg1995petrov <oleg1995petrov@yandex.by>
2021-10-25 05:05:50 +0300

    Update infrastructure

--- locals.tf
+++ locals.tf
@@ -10,10 +10,8 @@ locals {
   tags = {
     vpc = merge(var.common_tags, { Name = "VPC" })
     ig  = merge(var.common_tags, { Name = "Gateway" })
-    /* private_sn  = merge(var.common_tags, { Name = "Private subnet" }) */
     public_sn = merge(var.common_tags, { Name = "Public subnet" })
     public_rt = merge(var.common_tags, { Name = "Gateway route table" })
-    /* private_rt  = merge(var.common_tags, { Name = "Private route table" }) */
     lb          = merge(var.common_tags, { Name = "Load balancer" })
     lbsg        = merge(var.common_tags, { Name = "Load balancer security group" })
     lbtg        = merge(var.common_tags, { Name = "Load balancer target group" })
